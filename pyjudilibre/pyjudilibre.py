dd375e953eeb7ef85f9d30c989b3a2ac1841cf2c
pauldechorgnat <23296889+pauldechorgnat@users.noreply.github.com>
2023-05-31 17:28:06 +0200

    PYJUDILIBRE: Defining core class

---
+++ pyjudilibre/pyjudilibre.py
@@ -0,0 +1,17 @@
+class JudilibreClient:
+    """Class that implements a Python Client for the Judilibre API"""
+
+    def __init__(self, api_url: str, api_key_id: str):
+        pass
+
+    def search(self):
+        pass
+
+    def export(self):
+        pass
+
+    def get(self):
+        pass
+
+    def healthcheck(self):
+        pass
