5a8f5046626021b432694393a26b78504f66b563
pauldechorgnat <23296889+pauldechorgnat@users.noreply.github.com>
2023-06-01 00:00:00 +0200

    TEST: adding test_get

---
+++ tests/test_get.py
@@ -0,0 +1,50 @@
+import pytest
+from config import API_KEY_ID, API_URL
+
+from pyjudilibre import JudilibreClient
+from pyjudilibre.exceptions import (
+    JudilibreDecisionNotFoundError,
+    JudilibreWrongURLError,
+)
+
+
+def test_get():
+    decision_id = "5fca56cd0a790c1ec36ddc07"
+
+    client = JudilibreClient(api_url=API_URL, api_key_id=API_KEY_ID)
+
+    decision = client.get(decision_id=decision_id)
+
+    assert decision["id"] == decision_id
+
+
+def test_get_wrong_id():
+    decision_id = "obvisously_wrong_id"
+
+    client = JudilibreClient(api_url=API_URL, api_key_id=API_KEY_ID)
+
+    with pytest.raises(JudilibreDecisionNotFoundError):
+        client.get(decision_id=decision_id)
+
+
+def test_get_wrong_url():
+    decision_id = "5fca56cd0a790c1ec36ddc07"
+
+    client = JudilibreClient(
+        api_url="https://wrong_url.judilibre.com", api_key_id=API_KEY_ID
+    )
+
+    with pytest.raises(JudilibreWrongURLError):
+        client.get(decision_id=decision_id)
+
+
+def test_get_wrong_credentials():
+    decision_id = "5fca56cd0a790c1ec36ddc07"
+
+    client = JudilibreClient(api_url=API_URL, api_key_id="obviously_wrong_credentials")
+
+    with pytest.raises(JudilibreWrongURLError):
+        client.get(decision_id=decision_id)
+
+    with pytest.raises(JudilibreWrongURLError):
+        client.get(decision_id=decision_id)
