127108bb1bda762c76db63f3b5009ababff82bc1
pauldechorgnat <23296889+pauldechorgnat@users.noreply.github.com>
2023-06-10 17:00:12 +0200

    PYJUDILIBRE: adding references to __ini__

--- pyjudilibre/pyjudilibre.py
+++ pyjudilibre/pyjudilibre.py
@@ -6,16 +6,43 @@ from .exceptions import (
     JudilibreWrongURLError,
 )
 from .models import JudilibreDecision
+from .references import (
+    CA_DECISION_TYPES,
+    CA_LOCATIONS,
+    CA_THEMES,
+    CC_CHAMBERS,
+    CC_DECISION_TYPES,
+    CC_FORMATIONS,
+    CC_PUBLICATIONS,
+    CC_SOLUTIONS,
+    CC_THEMES,
+)
 
 
 class JudilibreClient:
     """Class that implements a Python Client for the Judilibre API"""
 
-    def __init__(self, api_url: str, api_key_id: str):
+    def __init__(self, api_url: str, api_key_id: str, action_on_check: str = "warning"):
         self.api_url = api_url
         self.api_key_id = api_key_id
+        self.__version__ = "0.0.1"
+        self.action_on_check = action_on_check
 
-        self.api_headers = {"KeyId": api_key_id}
+        self.cc_decision_type_values = CC_DECISION_TYPES
+        self.cc_decision_solution_values = CC_SOLUTIONS
+        self.cc_chamber_values = CC_CHAMBERS
+        self.cc_formation_values = CC_FORMATIONS
+        self.cc_mateer = CC_THEMES
+        self.cc_publication = CC_PUBLICATIONS
+
+        self.ca_decision_type_values = CA_DECISION_TYPES
+        self.ca_nac = CA_THEMES
+        self.ca_locations = CA_LOCATIONS
+
+        self.api_headers = {
+            "KeyId": api_key_id,
+            "User-Agent": f"pyJudilibre {self.__version__}",
+        }
 
     def search(self):
         pass
