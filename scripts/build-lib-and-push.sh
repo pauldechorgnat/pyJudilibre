e37c1cb7febe4149b77c11f2535101899baa0cd8
pauldechorgnat <23296889+pauldechorgnat@users.noreply.github.com>
2025-08-10 19:47:05 +0200

    PYPI: ajout des tokens en local

--- scripts/build-lib-and-push.sh
+++ scripts/build-lib-and-push.sh
@@ -1,11 +1,23 @@
 #! /usr/bin/bash
-pip install './lib[dev]'
-python -m build          
-twine check dist/*
+
+
+set -o allexport
+source .env
+set +o allexport
+
+# pip install '.[dev]'
+# python -m build
+# twine check dist/*
+
+
 # TestPyPI first:
-twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+twine upload --repository-url https://test.pypi.org/legacy/ dist/* --password $JUDILIBRE_TEST_PYPI_TOKEN --user '__token__' 
 
 # Verify install in fresh venv:
-# python -m venv /tmp/jcli && source /tmp/jcli/bin/activate
-# pip install -i https://test.pypi.org/simple/ pyjudilibre
-# python -c "from pyjudilibre import JudilibreClient; print('ok', Client)"
+python -m venv /tmp/jcli && source /tmp/jcli/bin/activate
+pip install -i https://test.pypi.org/simple/ pyjudilibre
+python -c "from pyjudilibre import JudilibreClient; print('ok', Client)"
+
+
+# Push to PyPI
+twine upload --repository-url https://test.pypi.org/legacy/ dist/* --password $JUDILIBRE_PYPI_TOKEN --user '__token__' 
