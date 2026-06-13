def644e89d62fda822e50aad919ec9639f721b1f
pauldechorgnat <23296889+pauldechorgnat@users.noreply.github.com>
2026-06-13 17:27:02 +0200

    OPS: chgt de références de la CI

--- docs/index.md
+++ docs/index.md
@@ -1,4 +1,4 @@
-# pyJudilibre `0.14.5`
+# pyJudilibre `0.14.6`
 
 ## Presentation
 
--- lib/pyjudilibre/pyjudilibre.py
+++ lib/pyjudilibre/pyjudilibre.py
@@ -36,7 +36,7 @@ from pyjudilibre.models import (
 from tqdm import TqdmExperimentalWarning
 from tqdm.autonotebook import tqdm
 
-__version__ = "0.14.5"
+__version__ = "0.14.6"
 
 warnings.filterwarnings("ignore", category=TqdmExperimentalWarning)
 
